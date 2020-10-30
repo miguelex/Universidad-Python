8760c923a9c20eb6385633f017a508660041c764
migue <miguelexd@hotmail.com>
2020-10-30 16:50:24 +0100

    constantes

---
+++ POO/modulos/constantes.py
@@ -0,0 +1,6 @@
+MI_CONSTANTE = "Valor de mi constante"
+
+class Matematicas:
+    PI = 3.1416
+#print(MI_CONSTANTE)
+#print(Matematicas.PI)
---
+++ POO/modulos/test_constante.py
@@ -0,0 +1,10 @@
+from constantes import *
+
+print(MI_CONSTANTE)
+print(Matematicas.PI)
+
+MI_CONSTANTE = "Nuevo valor"
+Matematicas.PI = 3.14
+
+print(MI_CONSTANTE)
+print(Matematicas.PI)
