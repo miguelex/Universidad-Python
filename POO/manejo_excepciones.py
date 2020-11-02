c7c5424571c07637da0404e64652894e551385d2
migue <miguelexd@hotmail.com>
2020-11-02 16:47:39 +0100

    Manejo de Excepciones 3

--- POO/manejo_excepciones.py
+++ POO/manejo_excepciones.py
@@ -1,14 +1,20 @@
 resultado = None
-a = "10"
-b = 0
 
 try:
+    a = int(input("Primer numero: "))
+    b = int(input("Segundo nuemo: "))
     resultado = a / b
 except ZeroDivisionError as e:
-    print("Ocurrio un error", e)
+    print("Ocurrio un error con AeroDivisionError", e)
     print(type(e))
 except TypeError as e:
-    print("Ocurrio un error", e)
+    print("Ocurrio un error con TyperError", e)
+    print(type(e))
+except ValueError as e:
+    print("Ocurrio un error con ValuesError", e)
+    print(type(e))
+except Exception as e:
+    print("Ocurrio un error con Exception", e)
     print(type(e))
     
 print("resultado: ", resultado)
