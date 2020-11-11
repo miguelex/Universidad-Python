04861b4d6a38f8b80b9ecd856af59404acec634c
migue <miguelexd@hotmail.com>
2020-11-11 12:05:51 +0100

    mundo pc 5

---
+++ mundo_pc/orden.py
@@ -0,0 +1,24 @@
+#from computadora import Computadora
+
+class Orden():
+    
+    contador_ordenes = 0
+    
+    def __init__(self, computadoras):
+        
+        Orden.contador_ordenes += 1
+        self.__id_orden = Orden.contador_ordenes
+        self.__computadoras = computadoras
+        
+    def agregar_computadora (self, computadora):
+        self.__computadoras.append(computadora)
+        
+    def __str__(self):
+        computadora_str = ""
+        for computadora in self.__computadoras:
+            computadora_str +=computadora.__str__()
+        
+        return (
+            f"Orden: {self.__id_orden}, "
+            f"Computadoras: {computadoras_str}, "
+        )
