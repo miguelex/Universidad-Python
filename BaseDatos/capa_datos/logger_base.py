d9eca2e88f5fd071e465a0004accbf3fc9c18a64
migue <miguelexd@hotmail.com>
2020-11-17 11:27:36 +0100

    logging 2

--- BaseDatos/capa_datos/logger_base.py
+++ BaseDatos/capa_datos/logger_base.py
@@ -1,11 +1,18 @@
 import logging
 
+# Variable logger a utilizar
 logger = logging
 
-logger.basicConfig(level = logging.DEBUG) # Solo para desarrollo o para descubrir un problema. En produccion ponerlo en INFO o WARNING
+logger.basicConfig(level = logging.DEBUG,
+                   format='%(asctime)s: %(levelname)s [%(filename)s:%(lineno)s] %(message)s',
+                   datefmt='%d/%m/%Y %I:%M:%S %p',
+                   handlers= [logging.FileHandler('capa_datos.log'),
+                              logging.StreamHandler()
+                              ]) # Solo para desarrollo o para descubrir un problema. En produccion ponerlo en INFO o WARNING
 
-logging.warning('mensaje nivel warning')
-logging.info('menaje nivel info')
-logging.debug('menaje nivel debug')
-logging.error('Ocurrio un error en la base de datos')
-logging.debug('Se realizo la conexión con éxito')
+if __name__ == '__main__':
+    logging.warning('mensaje nivel warning')
+    logging.info('mensaje nivel info')
+    logging.debug('mensaje nivel debug')
+    logging.error('Ocurrio un error en la base de datos')
+    logging.debug('Se realizo la conexión con éxito')
