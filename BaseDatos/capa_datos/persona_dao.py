39e90b271f80959d395925c03f0a399cb2b83491
migue <miguelexd@hotmail.com>
2020-11-18 12:19:08 +0100

    Actualización y Eliminación de Personas con la clase PersonaDAO

--- BaseDatos/capa_datos/persona_dao.py
+++ BaseDatos/capa_datos/persona_dao.py
@@ -10,6 +10,8 @@ class PersonaDao:
     
     __SELECCIONAR = 'SELECT * FROM persona ORDER BY id_persona'
     __INSERTAR = 'INSERT INTO persona(nombre, apellidos, email) VALUES (%s, %s, %s)'
+    __ACTUALIZAR = 'UPDATE persona SET nombre = %s, apellidos = %s, email = %s WHERE id_persona = %s'
+    __ELIMINAR = 'DELETE FROM persona WHERE id_persona = %s'
     
     @classmethod
     def seleccionar (cls):
@@ -40,12 +42,59 @@ class PersonaDao:
             logger.error(f'Excepcion al insertar persona: {e}')
         finally:
             Conexion.cerrar()
+    
+    @classmethod
+    def actualizar(cls, persona):
+        try:
+            conexion = Conexion.obtenerConexion()
+            cursor = Conexion.obtenerCursor()
+            logger.debug(cursor.mogrify(cls.__ACTUALIZAR))
+            logger.debug(f'Persona a actualizar: {persona}')
+            valores = (persona.get_nombre(), persona.get_apellidos(), persona.get_email(), persona.get_id_persona())
+            cursor.execute(cls.__ACTUALIZAR, valores)
+            conexion.commit()
+            return cursor.rowcount
+        except Exception as e:
+            conexion.rollback()
+            logger.error(f'Excepcion al actualizar persona: {e}')
+        finally:
+            Conexion.cerrar()
+            
+    @classmethod
+    def eliminar(cls, persona):
+        try:
+            conexion = Conexion.obtenerConexion()
+            cursor = Conexion.obtenerCursor()
+            logger.debug(cursor.mogrify(cls.__ELIMINAR))
+            logger.debug(f'Persona a eliminar: {persona}')
+            valores = (persona.get_id_persona(),)
+            cursor.execute(cls.__ELIMINAR, valores)
+            conexion.commit()
+            return cursor.rowcount
+        except Exception as e:
+            conexion.rollback()
+            logger.error(f'Excepcion al eliminar persona: {e}')
+        finally:
+            Conexion.cerrar()
         
 if __name__ == '__main__':
+    #Seleccioanr registros
     #personas = PersonaDao.seleccionar()
     #for persona in personas:
     #    logger.debug(persona)
     #    logger.info(persona.get_id_persona())
-    persona = Persona (nombre ="Pedro", apellidos="Najera", email ="pnajera@gmail.com")
-    registros_insertados = PersonaDao.insertar(persona)
-    logger.debug(f'Registros insertados: {registros_insertados}')
+    
+    #Insertar registro
+    #persona = Persona (nombre ="Pedro", apellidos="Najera", email ="pnajera@gmail.com")
+    #registros_insertados = PersonaDao.insertar(persona)
+    #logger.debug(f'Registros insertados: {registros_insertados}')
+    
+    #Actualziar un registro existente
+    #persona = Persona (2, 'Karla', 'Gomez', 'kgomez@mail.com')
+    #personas_actualizadas = PersonaDao.actualizar(persona)
+    #logger.debug(f'Personas actualizadas: {personas_actualizadas}') 
+    
+    #Eliminar persona
+    persona =Persona(id_persona=12)
+    persona_eliminada = PersonaDao.eliminar(persona)
+    logger.debug(f'Personas eliminadas: {persona_eliminada}')
