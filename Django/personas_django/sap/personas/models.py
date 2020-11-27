ec0b229afa671b7f318bb320bee03bb7b5e0e070
migue <miguelexd@hotmail.com>
2020-11-27 16:12:05 +0100

    Clase de Modelo desde la consola de Administración

--- Django/personas_django/sap/personas/models.py
+++ Django/personas_django/sap/personas/models.py
@@ -5,3 +5,6 @@ class Persona(models.Model):
     nombre = models.CharField(max_length=255)
     apellido = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
+
+    def __str__(self):
+        return  f'Persona {self.id}: {self.nombre} {self.apellido} {self.email}'
