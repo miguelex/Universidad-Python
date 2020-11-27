481d3872dc5b6a5185dfad335fe7ce5699d7e3e9
migue <miguelexd@hotmail.com>
2020-11-27 15:58:06 +0100

    Creación de Clases de Modelo en Python

---
+++ Django/personas_django/sap/personas/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class Persona(models.Model):
+    nombre = models.CharField(max_length=255)
+    apellido = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
