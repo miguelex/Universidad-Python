cb7c3072b484d05101b6b5395e01d86ec7e278f9
migue <miguelexd@hotmail.com>
2020-12-02 16:37:08 +0100

    Crud Flask

---
+++ Flask/SapFlask/app.py
@@ -0,0 +1,37 @@
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+# Configuracion de la BD
+
+USER_DB = 'postgres'
+PASS_DB = 'admin'
+URL_DB = 'localhost'
+NAME_BD = 'sap_flask_db'
+FULL_URL_DB = f'postgresql://{USER_DB}:{PASS_DB}@{URL_DB}/{NAME_BD}'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = FULL_URL_DB
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#Inicializacion del objeto db de sqlalchemy
+db = SQLAlchemy(app)
+
+#Configurar flask-migrate
+migrate = Migrate()
+migrate.init_app(app, db)
+
+class Persona(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(250))
+    apellido = db.Column(db.String(250))
+    email = db.Column(db.String(250))
+
+    def __str__(self):
+        return (
+            f'Id: {self.id}, '
+            f'Nombre: {self.nombre}, '
+            f'Apellido: {self.apellido}, '
+            f'Email: {self.email}, '
+        )
